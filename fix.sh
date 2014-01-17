68204a73d7a69a95e012e3ed61a00384945719eb
manaten <manaten@manaten.net>
2014-01-17 11:22:05 +0900

    † fix md †

--- fix.sh
+++ fix.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 for i in php javascript bash css java scala
 do
-  sed -i -e 's/(<div>)?\['$i'\]/```'$i'/g' *.md
-  sed -i -e 's/\[\/'$i'\](<\/div>)?/```/g' *.md
+  sed -i -e 's/\['$i'\]/```'$i'/g' *.md
+  sed -i -e 's/\[\/'$i'\]/```/g' *.md
 done
